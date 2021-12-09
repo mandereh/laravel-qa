5b6833938d81be4ea2f1d7e77789b109e5baa3f3
Daerego Braide <phronesis4xt@gmail.com>
2021-12-09 19:05:47 +0100

    prepopulate User and Question tables using model factory and seed

--- app/User.php
+++ app/User.php
@@ -38,6 +38,6 @@ class User extends Authenticatable
     ];
 
     public function questions(){
-        return $this->hasMany(Question::class)
+        return $this->hasMany(Question::class);
     }
 }
