cbe3d44b8a1dc114290aabac904087f5a1f6b487
Daerego Braide <phronesis4xt@gmail.com>
2021-12-15 13:31:06 +0100

    implement show functionality for the question

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('questions', 'QuestionsController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('questions', 'QuestionsController')->except('show');
+Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
