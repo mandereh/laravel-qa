2d966ad74fb553df0771e2cbe74e9a18948d983d
Daerego Braide <phronesis4xt@gmail.com>
2021-12-22 13:23:20 +0100

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -11,12 +11,23 @@ class Answer extends Model
         return $this->belongsTo(Question::class);
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($answer) {
+            $answer->question->increment('answers_count');
+        });
+
     }
 }
