439c81904081b482e536b2db0f9c0d0aa36049f8
Daerego Braide <phronesis4xt@gmail.com>
2021-12-27 20:28:11 +0100

    display answers for questions

--- app/Answer.php
+++ app/Answer.php
@@ -30,4 +30,8 @@ class Answer extends Model
         });
 
     }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -37,7 +37,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function questions(){
+    public function questions()
+    {
         return $this->hasMany(Question::class);
     }
 
@@ -46,8 +47,15 @@ class User extends Authenticatable
         // return route('questions.show', $this->id);
     }
 
-    public function answers(){
-
+    public function answers()
+    {
         return $this->hasMany(Answer::class);
+    }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 32;
+        return "https://www.gravatar.com/avatar/" . md5(strtolower(trim($email))) . "?s=" . $size;
     }
 }
