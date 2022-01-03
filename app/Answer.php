713bddfec78ad33d4e41ff6a86cad3540934639d
Daerego Braide <phronesis4xt@gmail.com>
2022-01-03 18:21:56 +0100

    implement accept the answer as best answer

--- app/Answer.php
+++ app/Answer.php
@@ -8,7 +8,7 @@ class Answer extends Model
 {
     protected $fillable=['body','user_id'];
 
-    public function question()
+    public function question() 
     {
         return $this->belongsTo(Question::class);
     }
@@ -40,6 +40,15 @@ class Answer extends Model
     }
 
     public function getStatusAttribute(){
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
+    }
+
+    public function getIsBestAttribute(){
+        return $this->isBest();
+    }
+
+    public function isBest()
+    {
+        return $this->id === $this->question->best_answer_id;
     }
 }
--- app/Policies/AnswerPolicy.php
+++ app/Policies/AnswerPolicy.php
@@ -80,4 +80,9 @@ class AnswerPolicy
     // {
     //     //
     // }
+
+    public function accept(User $user,Answer $answer){
+
+        return $user->id === $answer->question->user_id;
+    }
 }
