7bc85c540e157a3a61eb9692ffc702b586ea136c
Daerego Braide <phronesis4xt@gmail.com>
2022-01-10 18:00:51 +0100

    Implement DRY principle on the application views

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -18,55 +18,20 @@
                     </div>
                         <hr>
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                                {{-- upvote question --}}
-                            <a title="This question is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                            onclick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit();"
-                            >  <i class="fas fa-caret-up fa-2x"></i>
-                            </a>
-                            <form id="up-vote-question-{{ $question->id }}" action="/questions/{{ $question->id }}/vote" method="post" style="display: none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                                {{-- vote counts --}}
-                            <span class="votes-count">{{ $question->votes_count }}</span>
-                                {{-- downvote question --}}
-                            <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                            onclick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit();"
-                            >
-                                <i class="fas fa-caret-down fa-2x"></i>
-                            </a>
-                            <form id="down-vote-question-{{ $question->id }}" action="/questions/{{ $question->id }}/vote" method="post" style="display: none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                                {{-- Mark as favourite --}}
-                            <a title="Click to mark as favourite question(click again to undo)" 
-                            class="favourite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favourited' : '') }} "
-                            onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
-                            >
-                                <i class="fas fa-star fa-2x"></i>
-                                <span class="favourite-count">{{ $question->favorites_count }}</span>
-                            </a>
-                            <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="post" style="display: none;">
-                                @csrf
-                                @if ($question->is_favorited)
-                                    @method('DELETE')
-                                @endif
-                            </form>
-                        </div>
+                        @include('shared._vote',[
+                            'model' => $question
+                        ])
                         <div class="media-body">
                             {{ $question->body_html}}
-                            <div class="float-right">
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
+                                @include('shared._author',[
+                                'model' => $question,
+                                'label' => 'Asked'
+                                ])
                             </div>
-                            <span class="text-muted">Asked {{ $question->created_at }}</span>
-                            <div class="media mt-2">
-                                <a href="{{ $question->user->url }}" class="pr-2">
-                                    <img src="{{ $question->user->avatar }}" alt="" srcset="">
-                                </a>
-                                <div class="media-body mt-1">
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                </div>
                             </div>
                         </div>
                     </div>
---
+++ resources/views/shared/_vote.blade.php
@@ -0,0 +1,50 @@
+@if ($model instanceof App\Question)
+    @php
+        $name = 'question';
+        $firstURISegment = 'questions';
+    @endphp
+@elseif ($model instanceof App\Answer)
+
+    @php
+        $name = 'answer';
+        $firstURISegment = 'answers';
+    @endphp
+    
+@endif
+@php
+    $formId = $name ."-". $model->id;
+    $formAction = "/{ $firstURISegment }/{ $model->id }/vote";
+@endphp
+<div class="d-flex flex-column vote-controls">
+        {{-- upvote question --}}
+    <a title="This {{ $name }} is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+    onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();"
+    >  <i class="fas fa-caret-up fa-2x"></i>
+    </a>
+    <form id="up-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display: none;">
+        @csrf
+        <input type="hidden" name="vote" value="1">
+    </form>
+        {{-- vote counts --}}
+    <span class="votes-count">{{ $model->votes_count }}</span>
+        {{-- downvote question --}}
+    <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+    onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();"
+    >
+        <i class="fas fa-caret-down fa-2x"></i>
+    </a>
+    <form id="down-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display: none;">
+        @csrf
+        <input type="hidden" name="vote" value="-1">
+    </form>
+        {{-- Mark as favourite --}}
+        @if ($model instanceof App\Question)
+            @include('shared._favorite',[
+                'model' => $model
+            ])
+        @elseif($model instanceof App\Answer)
+            @include('shared._accept',[
+                'model' => $model
+            ])
+        @endif
+</div>
